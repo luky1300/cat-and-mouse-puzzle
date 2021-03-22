103a2e91eed49ebaf43fe0163c4b15566a21ff0a
luky1300 <58443403+luky1300@users.noreply.github.com>
2021-03-21 21:14:14 -0400

    tiles can be moved and sticking to the closest spot inside the grid

--- src/main.js
+++ src/main.js
@@ -1,40 +1,59 @@
 const config = {
-    width: 600,
-    height: 600,
-    type: Phaser.AUTO,
-    scene: {
-        preload: preload,
-        create: create
-    }
-}
+  width: 600,
+  height: 600,
+  type: Phaser.AUTO,
+  scene: {
+    preload: preload,
+    create: create,
+  },
+};
 
 const game = new Phaser.Game(config);
 
+function preload() {}
 
-function preload() {
-}
+function create() {
+  const field = this.add.rectangle(200, 200, 300, 300, 0x6666ff);
 
-function create(){
+  const tile1 = this.add
+    .rectangle(450, 200, 100, 100, 0x00ff00)
+    .setInteractive();
+  tile1.name = "tile1";
+  const tile2 = this.add
+    .rectangle(450, 450, 100, 100, 0x80aa90)
+    .setInteractive();
 
-    const field = this.add.rectangle(200, 200, 300, 300, 0x6666ff);
+  this.input.setDraggable(tile1);
+  this.input.setDraggable(tile2);
 
-    const tile1 = this.add.rectangle(450, 200, 100, 100, 0x00ff00).setInteractive();
+  let dragableOrigin = {};
 
-    this.input.setDraggable(tile1);
+  this.input.on("dragstart", function (pointer, gameObject) {
+    dragableOrigin.x = gameObject.x;
+    dragableOrigin.y = gameObject.y;
+  });
 
-    this.input.on('dragstart', function (pointer, gameObject) {
+  this.input.on("dragend", function (pointer, gameObject) {
+    const column = Math.round((gameObject.x - 100) / 100);
+    const row = Math.round((gameObject.y - 100) / 100);
 
-        this.children.bringToTop(gameObject);
-
-    }, this);
-
-    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-
-        gameObject.x = dragX;
-        gameObject.y = dragY;
-
-    });
-
-
+    console.log(gameObject);
+    if (
+      gameObject.x < 50 ||
+      gameObject.x > 350 ||
+      gameObject.y < 50 ||
+      gameObject.y > 350
+    ) {
+      gameObject.x = dragableOrigin.x;
+      gameObject.y = dragableOrigin.y;
+    } else {
+      gameObject.x = 100 + column * 100;
+      gameObject.y = 100 + row * 100;
     }
+  });
 
+  this.input.on("drag", function (pointer, gameObject, dragX, dragY) {
+    gameObject.x = dragX;
+    gameObject.y = dragY;
+  });
+}
