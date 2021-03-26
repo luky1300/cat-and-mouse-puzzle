73d3aad8cc8f366749fc8bf099fc4bd4051f0c07
luky1300 <58443403+luky1300@users.noreply.github.com>
2021-03-26 15:50:30 -0400

    tiles dragging and dropping correctly

--- src/main.js
+++ src/main.js
@@ -22,33 +22,74 @@ function create() {
   const tile2 = this.add
     .rectangle(450, 450, 100, 100, 0x80aa90)
     .setInteractive();
+  tile2.name = "tile2";
 
   this.input.setDraggable(tile1);
   this.input.setDraggable(tile2);
 
   let dragableOrigin = {};
+  let board = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
 
-  this.input.on("dragstart", function (pointer, gameObject) {
-    dragableOrigin.x = gameObject.x;
-    dragableOrigin.y = gameObject.y;
-  });
+  //TBD: refactor to account for board size and position
+  const isInsideBoard = (point) =>
+    point.x > 50 && point.x < 350 && point.y > 50 && point.y < 350;
+
+  const isOnEdge = (point) =>
+    point.x > 0 &&
+    point.x < 400 &&
+    point.y > 0 &&
+    point.y < 400 &&
+    !isInsideBoard(point);
+
+  this.input.on(
+    "dragstart",
+    function (pointer, gameObject) {
+      dragableOrigin.x = gameObject.x;
+      dragableOrigin.y = gameObject.y;
+      this.children.bringToTop(gameObject);
+    },
+    this
+  );
 
   this.input.on("dragend", function (pointer, gameObject) {
     const column = Math.round((gameObject.x - 100) / 100);
     const row = Math.round((gameObject.y - 100) / 100);
 
-    console.log(gameObject);
-    if (
-      gameObject.x < 50 ||
-      gameObject.x > 350 ||
-      gameObject.y < 50 ||
-      gameObject.y > 350
+    if (isInsideBoard(gameObject) && !board[row][column]) {
+      if (isInsideBoard(dragableOrigin)) {
+        //remove tile from original place
+        let startColumn = dragableOrigin.x / 100 - 1;
+        let startRow = dragableOrigin.y / 100 - 1;
+        board[startRow][startColumn] = 0;
+      }
+
+      //stick
+      gameObject.x = 100 + column * 100;
+      gameObject.y = 100 + row * 100;
+      board[row][column] = gameObject.name;
+    } else if (
+      (isInsideBoard(gameObject) && board[row] && board[row][column]) ||
+      isOnEdge(gameObject)
     ) {
+      //return to origin
       gameObject.x = dragableOrigin.x;
       gameObject.y = dragableOrigin.y;
     } else {
-      gameObject.x = 100 + column * 100;
-      gameObject.y = 100 + row * 100;
+      //leave where dropped
+
+      if (isInsideBoard(dragableOrigin)) {
+        //remove tile from original place
+        let startColumn = dragableOrigin.x / 100 - 1;
+        let startRow = dragableOrigin.y / 100 - 1;
+        board[startRow][startColumn] = 0;
+      }
+
+      gameObject.x;
+      gameObject.y;
     }
   });
 
