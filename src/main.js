f95e4fa9a167cf51d0b49d99d92450275344a123
luky1300 <58443403+luky1300@users.noreply.github.com>
2021-03-26 17:41:14 -0400

    tiles pictures added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const config = {
-  width: 600,
+  width: 800,
   height: 600,
   type: Phaser.AUTO,
   scene: {
@@ -11,25 +11,51 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("tile3", "assets/tile1.png");
+  
+  this.load.image("tile1", "assets/tile-corner.png");
+  this.load.image("tile2", "assets/tile-corner.png");
+  this.load.image("tile3", "assets/tile-corner.png");
+  this.load.image("tile4", "assets/tile-corner.png");
+  this.load.image("tile5", "assets/tile-flower.png");
+  this.load.image("tile6", "assets/tile-flower.png");
+  this.load.image("tile7", "assets/tile-two-corners.png");
+  this.load.image("tile8", "assets/tile-cross.png");
+  this.load.image("tile9", "assets/tile-bridge.png");
+
 }
 
 function create() {
   const field = this.add.rectangle(200, 200, 300, 300, 0x6666ff);
 
-  const tile1 = this.add
-    .rectangle(450, 200, 100, 100, 0x00ff00)
-    .setInteractive();
-  tile1.name = "tile1";
-  const tile2 = this.add
-    .rectangle(450, 450, 100, 100, 0x80aa90)
-    .setInteractive();
-  tile2.name = "tile2";
-  const tile3 = this.add.sprite(200, 450, "tile3").setInteractive();
+  const tile1 = this.add.sprite(425, 100, "tile1").setInteractive();
+  tile1.name = tile1;
+  const tile2 = this.add.sprite(550, 100, "tile2").setInteractive();
+  tile2.name = tile2;
+  const tile3 = this.add.sprite(675, 100, "tile3").setInteractive();
+  tile3.name = tile3;
+  const tile4 = this.add.sprite(425, 225, "tile4").setInteractive();
+  tile4.name = tile4;
+  const tile5 = this.add.sprite(425, 350, "tile5").setInteractive();
+  tile5.name = tile5;
+  const tile6 = this.add.sprite(550, 225, "tile6").setInteractive();
+  tile6.name = tile6;
+  const tile7 = this.add.sprite(550, 350, "tile7").setInteractive();
+  tile7.name = tile7;
+  const tile8 = this.add.sprite(675, 225, "tile8").setInteractive();
+  tile8.name = tile8;
+  const tile9 = this.add.sprite(675, 350, "tile9").setInteractive();
+  tile9.name = tile9;
+
 
   this.input.setDraggable(tile1);
   this.input.setDraggable(tile2);
   this.input.setDraggable(tile3);
+  this.input.setDraggable(tile4);
+  this.input.setDraggable(tile5);
+  this.input.setDraggable(tile6);
+  this.input.setDraggable(tile7);
+  this.input.setDraggable(tile8);
+  this.input.setDraggable(tile9);
 
   let dragableOrigin = {};
   let board = [
